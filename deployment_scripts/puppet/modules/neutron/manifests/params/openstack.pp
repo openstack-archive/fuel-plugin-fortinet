aa72447428de48139288501364ef12db1d297631
Jerry Zhao <jerryzhao@fortinet.com>
2017-07-12 13:17:27 -0700

    bump up networking-fortinet version to 1.1.3 to better support FOS5.4
    Change-Id: If95a93bf08faad4397fe9d5917aae165ca6e2ade

--- deployment_scripts/puppet/modules/neutron/manifests/params/openstack.pp
+++ deployment_scripts/puppet/modules/neutron/manifests/params/openstack.pp
@@ -17,7 +17,7 @@
 class neutron::params::openstack {
   $fgt_hash                    = hiera('fuel-plugin-fortinet')
 
-  $networking_fortinet_version = '1.1.1'
+  $networking_fortinet_version = '1.1.3'
 
   if($::osfamily == 'Redhat') {
     $fwaas_package      = 'python-neutron-fwaas'
